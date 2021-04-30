b659745130d0d47f07ac4ab8fef17256d31ee397
lboucaud <laurent.boucaud@carretechnologies.com>
2021-04-29 22:23:03 -0400

    todos

---
+++ TODOs.py
@@ -0,0 +1,13 @@
+# TODO resize
+# TODO fix end of file
+#   File "/Users/laurent/Documents/Perso/chess/chess2fen_explore/chess2fen_explore/c2f_explore/gui/annotation_gui.py", line 220, in get_next_frame
+#     next_frame = Image.fromarray(next(self.frame_generator))
+# StopIteration
+# TODO overlay box above buttons to explain
+# TODO revert last save
+# TODO popup when saved
+# TODO flash buttons when using keyboard bind
+# TODO save in csv (use Model from MVC pattern)
+# TODO pip package pgn to pngs
+# TODO new repo
+# TODO automatically generate .exe with github actions (nouveau repo)
