5de0793d64d601f4fd5fe9fb3d5e51302d10129f
lboucaud <laurent.boucaud@carretechnologies.com>
2021-05-16 14:06:28 -0400

    todos interactive fps

--- TODOs.py
+++ TODOs.py
@@ -1,6 +1,7 @@
 # TODO force aspect ratio when resizing window
 # TODO save in csv (use Model from MVC pattern)
 # TODO initialization scheme of database (create tables)
+# TODO interactively setup fps (video download) after providing url
 # TODO inner database
 # TODO docstrings
 # TODO pip package pgn to pngs
