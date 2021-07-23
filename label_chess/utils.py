dc78a4b75cff9ecd6e196703e12e40c445f88d9f
elbuco1 <laurent.boucaud@polymtl.ca>
2021-07-23 00:06:12 -0400

    fix problems

--- label_chess/utils.py
+++ label_chess/utils.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+from functools import lru_cache
 
 
 def frames_from_video_generator(video_path, fps_ratio):
@@ -31,6 +32,38 @@ def frames_from_video_generator(video_path, fps_ratio):
         else:
             break
     cap.release()
+
+def frame_id_generator(video, fps_ratio=1):
+    """ Generator of frame positions with a
+    fixed offset.
+    """
+    nb_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    for i in range(0, nb_frames, fps_ratio):
+        yield i 
+
+def get_video_frame(video, frame_id, last_frame_id = 0):
+    """ Get a specific frame from a video
+    Args:
+        video (cv2.VideoCapture)
+        frame_id (int): frame position
+        last_frame_id (int): last frame position pass to the function.
+            setting the position is expensive. this gives the possibility
+            to not set the pos.
+    """
+    if frame_id - last_frame_id != 1:
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+
+    ret, frame = video.read() 
+
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return frame
+    return None
+
+
+def load_video(video_path):
+    video = cv2.VideoCapture(video_path)
+    return video
 
 
 def resize_image(image, expected_height):
